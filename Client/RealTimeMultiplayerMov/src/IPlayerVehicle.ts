6d627b360c0072dce8c7ec62b8920b0dcb1c06f0
CarlosBlanco1 <97683431+CarlosBlanco1@users.noreply.github.com>
2024-11-18 10:08:10 -0700

    Part 1

---
+++ Client/RealTimeMultiplayerMov/src/IPlayerVehicle.ts
@@ -0,0 +1,11 @@
+interface PlayerVehicle {
+    id : Number,
+    xPosition : Number,
+    yPosition : Number,
+    Angle : Number,
+    isMovingForward : Boolean,
+    isTurningRight : Boolean,
+    isTurningLeft : Boolean,
+    isAccelerating : Boolean,
+    isDecelerating : Boolean,
+}
---
+++ Client/RealTimeMultiplayerMov/src/Vehicle.tsx
@@ -0,0 +1,25 @@
+import { useState } from "react";
+import CarIcon from "./CarIcon";
+
+export default function Vehicle() {
+  const [rotation, setRotation] = useState(0);
+  const [viewBox, setViewBox] = useState(0);
+  const [xPosition, setXPostion] = useState(0);
+  const [yPosition, setYPosition] = useState(0);
+  
+  return (
+    <div
+      style={{
+        position: "fixed",
+        rotate: `${rotation}deg`,
+        fill: "#999999",
+        width: `${viewBox}px`,
+        height: `${viewBox}px`,
+        top: `${xPosition}px`,
+        left: `${yPosition}px`,
+      }}
+    >
+      <CarIcon />
+    </div>
+  );
+}
